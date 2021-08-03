4978e93a70b1369045275228d3a08ee8c588278b
andreas chandra <phoenix.ichigo@yahoo.com>
2021-08-03 14:49:05 +0700

    change config, main, and options

--- options.py
+++ options.py
@@ -23,16 +23,22 @@ parser.add_argument('--test_model_path', type=str, default=None)
 ################
 # Dataset
 ################
-parser.add_argument('--dataset_code', type=str, default='ml-20m', choices=DATASETS.keys())
-parser.add_argument('--min_rating', type=int, default=4, help='Only keep ratings greater than equal to this value')
-parser.add_argument('--min_uc', type=int, default=5, help='Only keep users with more than min_uc ratings')
-parser.add_argument('--min_sc', type=int, default=0, help='Only keep items with more than min_sc ratings')
-parser.add_argument('--split', type=str, default='leave_one_out', help='How to split the datasets')
+parser.add_argument('--dataset_code', type=str,
+                    default='ml-1m', choices=DATASETS.keys())
+parser.add_argument('--min_rating', type=int, default=4,
+                    help='Only keep ratings greater than equal to this value')
+parser.add_argument('--min_uc', type=int, default=5,
+                    help='Only keep users with more than min_uc ratings')
+parser.add_argument('--min_sc', type=int, default=0,
+                    help='Only keep items with more than min_sc ratings')
+parser.add_argument('--split', type=str, default='leave_one_out',
+                    help='How to split the datasets')
 
 ################
 # Dataloader
 ################
-parser.add_argument('--dataloader_code', type=str, default='bert', choices=DATALOADERS.keys())
+parser.add_argument('--dataloader_code', type=str,
+                    default='bert', choices=DATALOADERS.keys())
 parser.add_argument('--dataloader_random_seed', type=float, default=0.0)
 parser.add_argument('--train_batch_size', type=int, default=64)
 parser.add_argument('--val_batch_size', type=int, default=64)
@@ -44,49 +50,68 @@ parser.add_argument('--test_batch_size', type=int, default=64)
 parser.add_argument('--train_negative_sampler_code', type=str, default='random', choices=['popular', 'random'],
                     help='Method to sample negative items for training. Not used in bert')
 parser.add_argument('--train_negative_sample_size', type=int, default=100)
-parser.add_argument('--train_negative_sampling_seed', type=int, default=None)
+parser.add_argument('--train_negative_sampling_seed', type=int, default=42)
 parser.add_argument('--test_negative_sampler_code', type=str, default='random', choices=['popular', 'random'],
                     help='Method to sample negative items for evaluation')
 parser.add_argument('--test_negative_sample_size', type=int, default=100)
-parser.add_argument('--test_negative_sampling_seed', type=int, default=None)
+parser.add_argument('--test_negative_sampling_seed', type=int, default=42)
 
 ################
 # Trainer
 ################
-parser.add_argument('--trainer_code', type=str, default='bert', choices=TRAINERS.keys())
+parser.add_argument('--trainer_code', type=str,
+                    default='bert', choices=TRAINERS.keys())
 # device #
-parser.add_argument('--device', type=str, default='cpu', choices=['cpu', 'cuda'])
+parser.add_argument('--device', type=str, default='cpu',
+                    choices=['cpu', 'cuda'])
 parser.add_argument('--num_gpu', type=int, default=1)
 parser.add_argument('--device_idx', type=str, default='0')
 # optimizer #
-parser.add_argument('--optimizer', type=str, default='Adam', choices=['SGD', 'Adam'])
+parser.add_argument('--optimizer', type=str,
+                    default='Adam', choices=['SGD', 'Adam'])
 parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
-parser.add_argument('--weight_decay', type=float, default=0, help='l2 regularization')
-parser.add_argument('--momentum', type=float, default=None, help='SGD momentum')
+parser.add_argument('--weight_decay', type=float,
+                    default=0, help='l2 regularization')
+parser.add_argument('--momentum', type=float,
+                    default=None, help='SGD momentum')
 # lr scheduler #
-parser.add_argument('--decay_step', type=int, default=15, help='Decay step for StepLR')
-parser.add_argument('--gamma', type=float, default=0.1, help='Gamma for StepLR')
+parser.add_argument('--decay_step', type=int, default=15,
+                    help='Decay step for StepLR')
+parser.add_argument('--gamma', type=float, default=0.1,
+                    help='Gamma for StepLR')
 # epochs #
-parser.add_argument('--num_epochs', type=int, default=100, help='Number of epochs for training')
+parser.add_argument('--num_epochs', type=int, default=100,
+                    help='Number of epochs for training')
 # logger #
 parser.add_argument('--log_period_as_iter', type=int, default=12800)
 # evaluation #
-parser.add_argument('--metric_ks', nargs='+', type=int, default=[10, 20, 50], help='ks for Metric@k')
-parser.add_argument('--best_metric', type=str, default='NDCG@10', help='Metric for determining the best model')
+parser.add_argument('--metric_ks', nargs='+', type=int,
+                    default=[10, 20, 50], help='ks for Metric@k')
+parser.add_argument('--best_metric', type=str, default='NDCG@10',
+                    help='Metric for determining the best model')
 
 ################
 # Model
 ################
-parser.add_argument('--model_code', type=str, default='bert', choices=MODELS.keys())
-parser.add_argument('--model_init_seed', type=int, default=None)
+parser.add_argument('--model_code', type=str,
+                    default='bert', choices=MODELS.keys())
+parser.add_argument('--model_init_seed', type=int, default=42,
+                    help='set seed')
 # BERT #
-parser.add_argument('--bert_max_len', type=int, default=None, help='Length of sequence for bert')
-parser.add_argument('--bert_num_items', type=int, default=None, help='Number of total items')
-parser.add_argument('--bert_hidden_units', type=int, default=None, help='Size of hidden vectors (d_model)')
-parser.add_argument('--bert_num_blocks', type=int, default=None, help='Number of transformer layers')
-parser.add_argument('--bert_num_heads', type=int, default=None, help='Number of heads for multi-attention')
-parser.add_argument('--bert_dropout', type=float, default=None, help='Dropout probability to use throughout the model')
-parser.add_argument('--bert_mask_prob', type=float, default=None, help='Probability for masking items in the training sequence')
+parser.add_argument('--bert_max_len', type=int, default=32,
+                    help='Length of sequence for bert')
+parser.add_argument('--bert_num_items', type=int, default=3533,
+                    help='Number of total items')
+parser.add_argument('--bert_hidden_units', type=int, default=32,
+                    help='Size of hidden vectors (d_model)')
+parser.add_argument('--bert_num_blocks', type=int, default=1,
+                    help='Number of transformer layers')
+parser.add_argument('--bert_num_heads', type=int, default=1,
+                    help='Number of heads for multi-attention')
+parser.add_argument('--bert_dropout', type=float, default=0.0,
+                    help='Dropout probability to use throughout the model')
+parser.add_argument('--bert_mask_prob', type=float, default=0.1,
+                    help='Probability for masking items in the training sequence')
 
 ################
 # Experiment
