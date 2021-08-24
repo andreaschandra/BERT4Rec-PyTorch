5ac7a8b0a9bfa83400c665d339cde8c69ed19cf3
andreas chandra <phoenix.ichigo@yahoo.com>
2021-08-24 13:43:43 +0700

    update main.py

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ def train():
     export_root = setup_train(args)
     model = model_factory(args)
     train_loader, val_loader, test_loader = dataloader_factory(args)
+
     trainer = trainer_factory(
         args, model, train_loader, val_loader, test_loader, export_root)
     trainer.train()
-    # test_result = test_with(trainer.best_model, test_loader)
-    # save_test_result(export_root, test_result)
 
 
 if __name__ == '__main__':
